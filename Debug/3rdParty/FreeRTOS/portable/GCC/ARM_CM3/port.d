2ea3bede49f728e97d300c7c9050f60e3a7e70d2
Yevgeniy M <m.yevgeniy.s@gmail.com>
2012-02-11 17:30:00 -0800

    some object files.  gitignore is not working for some reason

--- Debug/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/port.d
+++ Debug/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/port.d
@@ -1,42 +1,42 @@
 3rdParty/FreeRTOS/portable/GCC/ARM_CM3/port.d \
  3rdParty/FreeRTOS/portable/GCC/ARM_CM3/port.o: \
  ../3rdParty/FreeRTOS/portable/GCC/ARM_CM3/port.c \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/FreeRTOS.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/projdefs.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/FreeRTOSConfig.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/lpc17xx.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cm3.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cmInstr.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cmFunc.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/system_LPC17xx.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/portable.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/mpu_wrappers.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/task.h \
- /home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/list.h
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/FreeRTOS.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/projdefs.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/lpc17xx.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cm3.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cmInstr.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cmFunc.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/system_LPC17xx.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/portable.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/mpu_wrappers.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/task.h \
+ /Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/list.h
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/FreeRTOS.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/FreeRTOS.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/projdefs.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/projdefs.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/FreeRTOSConfig.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/lpc17xx.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/lpc17xx.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cm3.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cm3.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cmInstr.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cmInstr.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/CoreSupport/core_cmFunc.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/CoreSupport/core_cmFunc.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/system_LPC17xx.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/Core/CM3/DeviceSupport/NXP/LPC17xx/system_LPC17xx.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/portable.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/portable.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/mpu_wrappers.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/mpu_wrappers.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/task.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/task.h:
 
-/home/jenya/Documents/DEV/ee542/3rdParty/FreeRTOS/include/list.h:
+/Users/ymaksime/Documents/UW/EE542/huskyDrone/3rdParty/FreeRTOS/include/list.h:
